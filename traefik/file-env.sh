adb2a0db61ce77865b57866e54d6a5d02bd88606
webflo-dev <6934017+webflo-dev@users.noreply.github.com>
2020-10-09 18:04:01 +0000

    update traefik to handle all env start with FILE__

--- traefik/file-env.sh
+++ traefik/file-env.sh
@@ -5,21 +5,30 @@ set -o allexport
 
 file_env() {
 	local var="$1"
-	local fileVar="${var}_FILE"
+	local fileVar="FILE__${var}"
 	local def="${2:-}"
 	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
-		echo "Both $var and $fileVar are set (but are exclusive)"
+		echo "[env-init] Both $var and $fileVar are set (but are exclusive)"
 	fi
 	local val="$def"
 	if [ "${!var:-}" ]; then
 		val="${!var}"
 	elif [ "${!fileVar:-}" ]; then
-		val="$(< "${!fileVar}")"
+		if [ -e ${!fileVar} ]; then
+			val="$(< "${!fileVar}")"
+			echo "[env-init] Substitute done."
+		else
+			echo "[env-init] File not found for <${fileVar}>"
+		fi
 	fi
 	export "$var"="$val"
 	unset "$fileVar"
 }
 
-file_env "TRAEFIK_CERTIFICATESRESOLVERS_LETSENCRYPT_ACME_EMAIL"
+for varname in ${!FILE__*}
+do
+	echo "[env-init] Starting substitute for <$varname>"
+	file_env ${varname//FILE__/}
+done
 
 traefik "$@"
